8d2d5cb6d1b0afae31f94f526d7e6f6e68bda362
brrshh <brrshh@gmail.com>
2016-03-15 17:16:50 +0300

    second commit 2

--- src/test/java/bel/test/configuration/Config.java
+++ src/test/java/bel/test/configuration/Config.java
@@ -1,24 +1,16 @@
-package bel.test;
+package bel.test.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by Barys_Shliaha on 3/15/2016.
  */
-@EnableAutoConfiguration
-@RestController
+@SpringBootApplication
 public class Config {
-
-    @RequestMapping("/")
-    String home() {
-        return "Hello , it is home page";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Config.class, args);
     }
-
 }
---
+++ src/test/java/bel/test/configuration/ControllerConfiguration.java
@@ -0,0 +1,12 @@
+package bel.test.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Barys_Shliaha on 3/15/2016.
+ */
+@Configuration
+@ComponentScan("bel.test.controller")
+public class ControllerConfiguration {
+}
